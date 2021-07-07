4ce71a9cb1d3f5b16b527dba1d2ec90d6d0dab5c
LTB Lizzy <allenderse@gmail.com>
2021-07-07 21:27:43 +1000

    attempt to add remove-row function to table

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,5 @@
-var coinPortfolio = [];
-
-// init page
-function initPage(){
-  let tableEl = document.querySelector('#coin-table')
-  let clearFormBtn = document.querySelector('#clear-form-button');
-  coinPortfolio = getSavedCoinPortfolio();
-  generateCoinTable(coinPortfolio, tableEl);
-  initAvailbleCoins();
-  connectHandleClearForm(clearFormBtn);
-}
-
-initPage();
+$(document).ready(function(){
+    $("#coin-table").on('click', '.fa fa-pencil', function() {
+        $(this).closest('tr').remove();
+    });
+});
